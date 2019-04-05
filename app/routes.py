c8f9cae224db24da83376c973c655b48ab00a9fe
Ubuntu <ubuntu@ip-172-31-29-19.us-west-1.compute.internal>
2019-04-05 23:27:23 +0000

    First commit: basic flask app that get and displays an image.

---
+++ app/routes.py
@@ -0,0 +1,37 @@
+from flask import render_template, redirect, url_for, request, send_from_directory, flash
+from app import app
+import os
+from werkzeug import secure_filename
+
+@app.route('/<filename>')
+def get_file(filename):
+    return send_from_directory('static',filename)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            save_to=(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(save_to)
+            return render_template('displayFile.html', filename=filename)
+    return render_template('index.html')
+
+# allowed image types
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+app.config['ALLOWED_EXTENSIONS']=ALLOWED_EXTENSIONS
+
+# is file allowed to be uploaded?
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
