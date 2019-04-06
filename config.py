b160806af9f69b2a318c4661369c5ec6bcb98451
Ubuntu <ubuntu@ip-172-31-29-19.us-west-1.compute.internal>
2019-04-06 21:44:55 +0000

    fix config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     UPLOAD_FOLDER = 'static/'
-    LOC_MODELS_FOLDER = 'app/'
+    LOC_MODELS_FOLDER = '/home/ubuntu/cs121/app/'
