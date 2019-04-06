128ece9af28b238e894de673cb658a1e11a074c7
Ubuntu <ubuntu@ip-172-31-29-19.us-west-1.compute.internal>
2019-04-06 22:08:56 +0000

    fix location of fastai learn module again

--- config.py
+++ config.py
@@ -3,4 +3,3 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     UPLOAD_FOLDER = 'static/'
-    LOC_MODELS_FOLDER = '/home/ubuntu/cs121/app/'
