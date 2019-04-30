928c3ca942de1ea6e8c591f76a03ce284083409e
soanders <soanders@students.pitzer.edu>
2019-04-29 21:41:01 -0700

    Doing Stuff

--- app/routes.py
+++ app/routes.py
@@ -27,7 +27,7 @@ def upload_file():
             save_to=(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             file.save(save_to)
             pred_class=predictor.model_predict(save_to, '/home/ubuntu/cs121/app')
-            pred_URL = get_csv_URL('/home/ubuntu/cs121/static/assets/style_era.csv',pred_class)
+            pred_URL = get_csv_URL('style_era.csv',pred_class)
             return render_template('displayResult.html', filename=filename, prediction=pred_class, ArtURL=pred_URL)
     return render_template('index.html')
 
