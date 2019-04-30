cb5fd7dd054e3006eec36be50f56f465bc0347ac
Ubuntu <ubuntu@ip-172-31-39-217.ec2.internal>
2019-04-30 17:12:27 +0000

    small directory cleanup

--- app/models/script.sh
+++ app/models/script.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 echo "Geting fastai model"
-curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=1-90WaE4SnAgrZzLA4av2-G5rNlP07Bxv" > /tmp/intermezzo.html
-curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > stage-2.pth
+curl -c /tmp/cookies "https://drive.google.com/open?id=1iv_6OMZ94tm9auDlrYZ94WQ1QiQrKF9z" > /tmp/intermezzo.html
+curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > stage-2-50.pkl
 echo "Done!"
