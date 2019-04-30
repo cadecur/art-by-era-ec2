1d1f1e3121f07f7b5cf5e41ae27617641f9572fa
soanders <soanders@students.pitzer.edu>
2019-04-29 22:29:20 -0700

    Doing Stuff

--- app/routes.py
+++ app/routes.py
@@ -27,7 +27,7 @@ def upload_file():
             save_to=(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             file.save(save_to)
             pred_class=predictor.model_predict(save_to, '/home/ubuntu/cs121/app')
-            pred_URL = convert_to_url()
+            pred_URL = convert_to_url(pred_class)
             return render_template('displayResult.html', filename=filename, prediction=pred_class, ArtURL= pred_URL)
     return render_template('index.html')
 
@@ -40,8 +40,8 @@ def upload_file():
 #                 return row['Wikiart']
 #         return "Unknown"
 
-def convert_to_url():
-    ourString = "Butt Cheeks"
+def convert_to_url(pred_class):
+    ourString = str(pred_class)
     ourString = ourString.replace(' ','-')
     return('https://www.wikiart.org/en/paintings-by-style/' + ourString)
 
